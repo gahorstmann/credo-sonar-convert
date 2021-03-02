4e22ed564cf12cd487e0c79f8d061e0a5f7a726e
Gabriel Horstmann <gabriel.horstmann@deliverycenter.com>
2021-03-02 13:58:46 -0300

    adjusted when credo comes empty

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 var fs = require('fs');
 
-function severity(item){
+function severity(item) {
   var result;
   switch (true) {
     case (item >= 20):
@@ -22,7 +22,7 @@ function severity(item){
   return result;
 }
 
-function type(item){
+function type(item) {
   var result;
   switch (item) {
     case "warnings":
@@ -44,46 +44,49 @@ try {
   console.log(`Output: ${outputfile}!`);
   const filePath = process.env['GITHUB_WORKSPACE'] || '';
 
-  //TEST LOCAL
-  // var filePath = "./test";
-  // var inputfile = "credo_result.json";
   var json = require(filePath + "/" + inputfile);
-  var out = '{ "issues" : ['
-  for(var k in json.issues) {
-    var credo = json.issues[k];
-    if(credo.column){
-      var startColumn = credo.column-1
-      var endColumn = credo.column_end-1
-    }else{
-      startColumn = null
-      endColumn = null
-    }
-    var issue = {
-      engineId: credo.check,
-      ruleId: credo.scope,
-      severity: severity(credo.priority),
-      type: type(credo.category),
-      primaryLocation: {
-        message: credo.message,
-        filePath: credo.filename,
-        textRange: {
-          startLine: credo.line_no,
-          startColumn: startColumn,
-          endColumn: endColumn
+  if (json.issues[0] == null) {
+    fs.copyFile(filePath + "/" + inputfile, filePath + "/" + outputfile, (err) => {
+      if (err) throw err;
+    });
+  } else {
+    var out = '{ "issues" : ['
+    for (var k in json.issues) {
+      var credo = json.issues[k];
+      if (credo.column) {
+        var startColumn = credo.column - 1
+        var endColumn = credo.column_end - 1
+      } else {
+        startColumn = null
+        endColumn = null
+      }
+      var issue = {
+        engineId: credo.check,
+        ruleId: credo.scope,
+        severity: severity(credo.priority),
+        type: type(credo.category),
+        primaryLocation: {
+          message: credo.message,
+          filePath: credo.filename,
+          textRange: {
+            startLine: credo.line_no,
+            startColumn: startColumn,
+            endColumn: endColumn
+          }
         }
       }
+      var jsonString = JSON.stringify(issue);
+      out = out + jsonString + ','
     }
-    var jsonString= JSON.stringify(issue);
-    out = out + jsonString + ','
-  }
-  out = out.slice(0, -1) + ']}'
-  var obj = JSON.parse(out);
-  console.log(obj)
+    out = out.slice(0, -1) + ']}'
+    var obj = JSON.parse(out);
+    console.log(obj)
 
-  fs.writeFile(filePath + "/" + outputfile, out, function (err) {
-    if (err) throw err;
-    console.log("File "+ outputfile + " generated!");
-  });
+    fs.writeFile(filePath + "/" + outputfile, out, function (err) {
+      if (err) throw err;
+      console.log("File " + outputfile + " generated!");
+    });
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
