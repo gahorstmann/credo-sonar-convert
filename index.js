0cac61a4bac7a3af4634dc66e5c1f08cf48c4877
Gabriel Horstmann <gabriel.ah@hotmail.com>
2021-05-03 15:37:11 -0300

    ruleid null

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ try {
       }
       var issue = {
         engineId: credo.check,
-        ruleId: credo.scope,
+        ruleId: credo.scope || credo.check,
         severity: severity(credo.priority),
         type: type(credo.category),
         primaryLocation: {
